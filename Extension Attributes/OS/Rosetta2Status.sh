a2fecf75b2c067d7dbed933718465219e9329073
pwalker1485 <pwalker1485@icloud.com>
2022-03-02 22:37:06 +0000

    Updated Aug 2021 to use latest method

--- Extension Attributes/OS/Rosetta2Status.sh
+++ Extension Attributes/OS/Rosetta2Status.sh
@@ -4,37 +4,29 @@
 #                     Rosetta 2 Install Status - EA                    #
 #################### Written by Phil Walker Dec 2020 ###################
 ########################################################################
-# Edit July 2021
+# Edit Aug 2021
 
 ########################################################################
 #                            Variables                                 #
 ########################################################################
 
-# Intel CPU check
-intelCPU=$(sysctl -n machdep.cpu.brand_string | grep -o "Intel")
-# Rosetta 2 Launch Daemon (Pre 11.5)
-launchDaemonOld="/Library/Apple/System/Library/LaunchDaemons/com.apple.oahd.plist"
-# Rosetta 2 Launch Daemon
-launchDaemon="/System/Library/LaunchDaemons/com.apple.oahd.plist"
+# CPU Architecture
+cpuArch=$(/usr/bin/arch)
 
 ########################################################################
 #                         Script starts here                           #
 ########################################################################
 
-# Check or an Intel CPU
-if [[ -n "$intelCPU" ]]; then
-    ### DEBUG
-    #echo "Intel CPU detected, Rosetta 2 not required"
-    rosettaStatus="Not Required"
-else
-    ### DEBUG
-    #echo "Apple silicon Mac, checking Rosetta 2 install status"
-    # Check for the Rosetta 2 Launch Daemon
-    if [[ -f "$launchDaemon" ]] || [[ -f "$launchDaemonOld" ]]; then
-        rosettaStatus="Installed"
+if [[ "$cpuArch" == "arm64" ]]; then
+    arch -x86_64 /usr/bin/true 2>/dev/null
+    commandResult="$?"
+    if [[ "$commandResult" -eq "1" ]]; then
+        rosettaStatus="Not Installed"
     else
-    	rosettaStatus="Not Installed"
+        rosettaStatus="Installed"
     fi
+else
+    rosettaStatus="Not Required"
 fi
 echo "<result>${rosettaStatus}</result>"
 exit 0
