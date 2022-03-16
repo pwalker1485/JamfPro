f38c4911915f09994cf5e251741a1f86a10d154e
pwalker1485 <pwalker1485@icloud.com>
2022-03-16 17:20:53 +0000

    Updated to latest method

--- Scripts/Adobe/Kill All Adobe Processes and Services.sh
+++ Scripts/Adobe/Kill All Adobe Processes and Services.sh
@@ -4,6 +4,7 @@
 #                Kill All Adobe Processes and Services                 #
 ################### Written by Phil Walker Aug 2020 ####################
 ########################################################################
+# Modified Mar 2022
 
 ########################################################################
 #                            Variables                                 #
@@ -15,6 +16,20 @@ loggedInUser=$(stat -f %Su /dev/console)
 loggedInUserID=$(id -u "$loggedInUser")
 
 ########################################################################
+#                            Functions                                 #
+########################################################################
+
+function runAsUser ()
+{  
+# Run commands as the logged in user
+if [[ "$loggedInUser" == "" ]] || [[ "$loggedInUser" == "root" ]]; then
+    echo "No user logged in, unable to run commands as a user"
+else
+    launchctl asuser "$loggedInUserID" sudo -u "$loggedInUser" "$@"
+fi
+}
+
+########################################################################
 #                         Script starts here                           #
 ########################################################################
 
@@ -22,20 +37,26 @@ if [[ "$loggedInUser" == "" ]] || [[ "$loggedInUser" == "root" ]]; then
     echo "No user logged in"
 else
     # Get all user Adobe Launch Agents PIDs
-    userPIDs=$(su -l "$loggedInUser" -c "/bin/launchctl list | grep adobe" | awk '{print $1}')
+    userPIDs=$(runAsUser launchctl list | grep "adobe" | awk '{print $1}')
     # Kill all processes
     if [[ "$userPIDs" != "" ]]; then
         while IFS= read -r line; do
-            kill -9 "$line" 2>/dev/null
+            kill -9 "$line" &>/dev/null
         done <<< "$userPIDs"
     fi
-    # Bootout all user Adobe Launch Agents
-    launchctl bootout gui/"$loggedInUserID" /Library/LaunchAgents/com.adobe.* 2>/dev/null
-    # Bootout Adobe Launch Daemons
-    launchctl bootout system /Library/LaunchDaemons/com.adobe.* 2>/dev/null
-    pkill -9 "obe"
+    launchAgents=$(find "/Library/LaunchAgents" -iname "com.adobe*" -type f -maxdepth 1)
+    if [[ "$launchAgents" != "" ]]; then
+        # Bootout all user Adobe Launch Agents
+        launchctl bootout gui/"$loggedInUserID" /Library/LaunchAgents/com.adobe.* &>/dev/null
+    fi
+    launchDaemons=$(find "/Library/LaunchDaemons" -iname "com.adobe*" -type f -maxdepth 1)
+    if [[ "$launchDaemons" != "" ]]; then
+        # Bootout Adobe Launch Daemons
+        launchctl bootout system /Library/LaunchDaemons/com.adobe.* &>/dev/null
+    fi
+    pkill -9 "obe" &>/dev/null
     sleep 5
     # Close any Adobe Crash Reporter windows (e.g. Bridge)
-    pkill -9 "Crash Reporter"
+    pkill -9 "Crash Reporter" &>/dev/null
 fi
 exit 0
