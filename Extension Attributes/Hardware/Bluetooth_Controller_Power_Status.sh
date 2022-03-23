590e9e821f71af7c1ac2b3494b21222b038c02ca
pwalker1485 <pwalker1485@icloud.com>
2022-03-23 23:33:32 +0000

    Updated for Monterey support

--- Extension Attributes/Hardware/Bluetooth_Controller_Power_Status.sh
+++ Extension Attributes/Hardware/Bluetooth_Controller_Power_Status.sh
@@ -1,19 +1,35 @@
-#!/bin/bash
+#!/bin/zsh
 
 ########################################################################
-#              Bluetooth Controller Power Status - EA                  #
+#                 Bluetooth Controller Power State                     #
 ################## Written by Phil Walker July 2019 ####################
 ########################################################################
+# Edit Jan 2022
 
-btPowerStatus=$(/usr/libexec/PlistBuddy -c "print ControllerPowerState" /Library/Preferences/com.apple.Bluetooth.plist)
+# Load is-at-least
+autoload is-at-least
+########################################################################
+#                            Variables                                 #
+########################################################################
+
+# OS product version
+osVersion=$(sw_vers -productVersion)
+# Monterey major version
+montereyMajor="12"
 
 ########################################################################
 #                         Script starts here                           #
 ########################################################################
 
-if [[ "$btPowerStatus" == "1" ]] || [[ "$btPowerStatus" == "true" ]]; then
-    echo "<result>On</result>"
+if is-at-least "$montereyMajor" "$osVersion"; then
+    btPowerState=$(system_profiler SPBluetoothDataType | awk '/State/ {print $NF}')
 else
-    echo "<result>Off</result>"
+    controllerPowerState=$(/usr/libexec/PlistBuddy -c "print ControllerPowerState" /Library/Preferences/com.apple.Bluetooth.plist 2>/dev/null)
+    if [[ "$controllerPowerState" == "1" ]] || [[ "$controllerPowerState" == "true" ]]; then
+        btPowerState="On"
+    else
+        btPowerState="Off"
+    fi
 fi
+echo "<result>${btPowerState}</result>"
 exit 0
